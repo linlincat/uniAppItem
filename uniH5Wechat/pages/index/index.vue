bfb0b4c7ff92d70d60fc21413737473252e1b441
cat <none>
2023-03-16 15:41:28 +0800

    首页导航布局

--- uniH5Wechat/pages/index/index.vue
+++ uniH5Wechat/pages/index/index.vue
@@ -1,34 +1,78 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="m-content">
+		<view class="status_bar"
+			style="background-image: linear-gradient(to right, rgb(245, 191, 75), rgb(247, 215, 88));"></view>
+
+		<!-- #ifdef MP-WEIXIN -->
+		<view style="background-image: linear-gradient(to right, rgb(245, 191, 75), rgb(247, 215, 88));">
+			<uni-search-bar placeholder="今天星期四" cancel-text="取消">
+				<uni-icons slot="searchIcon" color="#999999" size="18" type="home" />
+			</uni-search-bar>
 		</view>
+		<!-- #endif -->
+		<!-- #ifdef H5 -->
+		<view class="search-box">
+			<!-- 首页顶部搜索 -->
+			<uni-search-bar placeholder="今天等文章有新款哈" cancel-text="取消">
+				<uni-icons slot="searchIcon" color="#999999" size="18" type="home" />
+			</uni-search-bar>
+		</view>
+		<!-- #endif -->
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script setup>
+	// 引入 
+	import {
+		onReady,
+		onShow,
+		onLoad
+	} from '@dcloudio/uni-app';
+	onLoad(() => {
+		console.log("load")
+	})
+	onReady(() => {
+		// #ifdef  MP-WEIXIN
+		// 需条件编译的代码
+		// #endif
+		// let nihao = uni.getMenuButtonBoundingClientRect()
+		// // let menuButtonInfo = uni.getMenuButtonBoundingClientRect()
+		// console.log("nihao")
+	})
 </script>
 
-<style>
-	.content {
+<style lang="scss">
+	page {
+		// 16位的颜色不出来
+	}
+
+	.m-content {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		position: relative;
+		/*  #ifdef  MP-WEIXIN  */
+		:deep(.uni-searchbar) {
+			width: 65%;
+		}
+		/*  #endif  */
+
+		.status_bar {
+			height: var(--status-bar-height);
+			width: 100%;
+		}
+
+		.search-box {
+			width: 100%;
+			background-image: linear-gradient(to right, rgb(245, 191, 75), rgb(247, 215, 88));
+		}
+
+		.search-width {
+			width: 70%;
+		}
+
+		:deep(.uni-searchbar__box) {
+			justify-content: start;
+		}
 	}
 
 	.logo {
@@ -38,15 +82,5 @@
 		margin-left: auto;
 		margin-right: auto;
 		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
 	}
 </style>
---
+++ uniH5Wechat/unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,37 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+if (!Array) {
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  const _easycom_uni_search_bar2 = common_vendor.resolveComponent("uni-search-bar");
+  (_easycom_uni_icons2 + _easycom_uni_search_bar2)();
+}
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+const _easycom_uni_search_bar = () => "../../node-modules/@dcloudio/uni-ui/lib/uni-search-bar/uni-search-bar.js";
+if (!Math) {
+  (_easycom_uni_icons + _easycom_uni_search_bar)();
+}
+const _sfc_main = {
+  __name: "index",
+  setup(__props) {
+    common_vendor.onLoad(() => {
+      console.log("load");
+    });
+    common_vendor.onReady(() => {
+    });
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.p({
+          color: "#999999",
+          size: "18",
+          type: "home"
+        }),
+        b: common_vendor.p({
+          placeholder: "今天星期四",
+          ["cancel-text"]: "取消"
+        })
+      };
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "/Users/xiaopingguo/Desktop/nowDown/uniAppItem/uniH5Wechat/pages/index/index.vue"]]);
+wx.createPage(MiniProgramPage);
